7e380adff44d4b44689d6920ff7f77e78f8c0552
Nick Beattie <nick@nickbytes.com>
2016-09-23 01:43:24 -0700

    Add entry point
    This is where I’ll set up react, react-router, and redux.
    
    The app will have two page templates:
    1. UserPage – Where I will show a list of employees by their usernames.
    2. SinglePage – A page showing a single user based on router params.

---
+++ client/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { render } from 'react-dom';
+
+
+// Import Components
+import App from './components/App';
+import SinglePage from './components/SinglePage';
+import UserPage from './components/UserPage';
+
+// import react router deps
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store, { history } from './store';
+
+const router = (
+  <Provider store={store}>
+    <Router history={history}>
+      <Route path="/" component={App}>
+        <IndexRoute component={UserPage}></IndexRoute>
+        <Route path="/:username" component={SinglePage}></Route>
+      </Route>
+    </Router>
+  </Provider>
+)
+
+render(router, document.getElementById('root'));
