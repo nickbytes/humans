f3b12589546c09914ea6c0d1c15a5954b528f6b7
Nick Beattie <nick@nickbytes.com>
2016-09-23 03:58:03 -0700

    Add user data

--- client/data/employees.js
+++ client/data/employees.js
@@ -1,28 +1,35 @@
 const employees = [
    {
       "username":"nickbytes",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e35/12552326_495932673919321_1443393332_n.jpg",
-      "title":"Software Enginerd1"
+      "display_src":"http://placekitten.com/200/200",
+      "title":"Software Engineer",
+      "first":"Jane",
+      "last":"Doe",
+      "location":"New York City"
    },
    {
       "username":"littlegrowler",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/12407344_1283694208323785_735653395_n.jpg",
-      "title":"Software Enginerd2"
+      "display_src":"http://placekitten.com/200/200",
+      "title":"Designer",
+      "first":"Sam",
+      "last":"Doe",
+      "location":"San Francisco"
    },
    {
       "username":"lax2208x",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xpf1/t51.2885-15/e35/923995_1704188643150533_1383710275_n.jpg",
-      "title":"Software Enginerd3"
+      "display_src":"http://placekitten.com/200/200",
+      "title":"Writer",
+      "first":"Bob",
+      "last":"Doe",
+      "location":"Portland"
    },
    {
       "username":"syracuse512",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e35/12407480_1654828594805097_152207166_n.jpg",
-      "title":"Software Enginerd4"
-   },
-   {
-      "username":"laxZeppelin3",
-      "display_src":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e35/11875511_1562439187344831_813588280_n.jpg",
-      "title":"Software Enginerd5"
+      "display_src":"http://placekitten.com/200/200",
+      "title":"Director",
+      "first":"Deborah",
+      "last":"Doe",
+      "location":"Chicago"
    }
 ];
 
