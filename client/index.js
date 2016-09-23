407d676c6798558fa6c8b79cb8a5a5f99dd75c83
Nick Beattie <nick@nickbytes.com>
2016-09-23 09:04:54 -0700

    Import our new styles, add ace
    Ace is the full-fledged, app-ready version of Basscss. It includes almost all common used Basscss plugins, and some extras (like colors).

--- client/index.js
+++ client/index.js
@@ -8,6 +8,8 @@ import App from './components/App';
 import SinglePage from './components/SinglePage';
 import UserPage from './components/UserPage';
 
+import css from './styles/styles.css';
+
 // import react router deps
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { Provider } from 'react-redux';
