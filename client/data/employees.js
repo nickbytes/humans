b1aaf32deb33abbe8af26b1c5644034f6c5f92d9
Nick Beattie <nick@nickbytes.com>
2016-09-23 01:57:51 -0700

    Sample employees data

---
+++ client/data/employees.js
@@ -0,0 +1,30 @@
+const employees = [
+   {
+      "username":"nickbytes",
+      "display_src":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e35/12552326_495932673919321_1443393332_n.jpg",
+      "title":"Software Enginerd1"
+   },
+   {
+      "username":"littlegrowler",
+      "display_src":"https://scontent.cdninstagram.com/hphotos-xaf1/t51.2885-15/e35/12407344_1283694208323785_735653395_n.jpg",
+      "title":"Software Enginerd2"
+   },
+   {
+      "username":"lax2208x",
+      "display_src":"https://scontent.cdninstagram.com/hphotos-xpf1/t51.2885-15/e35/923995_1704188643150533_1383710275_n.jpg",
+      "title":"Software Enginerd3"
+   },
+   {
+      "username":"syracuse512",
+      "display_src":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e35/12407480_1654828594805097_152207166_n.jpg",
+      "title":"Software Enginerd4"
+   },
+   {
+      "username":"laxZeppelin3",
+      "display_src":"https://scontent.cdninstagram.com/hphotos-xap1/t51.2885-15/e35/11875511_1562439187344831_813588280_n.jpg",
+      "title":"Software Enginerd5"
+   }
+];
+
+
+export default employees;
